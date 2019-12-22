bd4216b77d470e95bc925761cbb9b1100f458f6e
omfgtora <spaminol@gmail.com>
2019-12-21 22:51:41 -0800

    Clean

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Draggable from 'react-draggable'
-import Clock from 'clock-react'
+import Clock from './components/Clock'
 import Menu from './components/Menu'
 import Doge from './assets/images/doge.png'
 import { 
@@ -21,39 +21,42 @@ const ResetStyles = createGlobalStyle`
   ${reset}
 `;
 
-const HourglassStyles = {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginTop: '45vh'
-}
-
 function App() {
 
   return (
     <div className="App">
       <ResetStyles />
       <ThemeProvider theme={themes.default}>
-      <Draggable positionOffset={{ x: '35vw', y: '25vh' }}>
-        <Window style={{ width: 400 }}>
-          <WindowHeader>Welcome to ToRA95</WindowHeader>
-          <Toolbar></Toolbar>
-          <WindowContent style={{ display: 'flex', justifyContent: 'center', flexDirecion: 'column', flexWrap: 'wrap' }}>
-            <img alt='Such Amaze' src={ Doge } draggable={ false } />
-            <div style={{ padding: '1rem 0'}}>Wow. Such retro.</div>
-          </WindowContent>
-        </Window>
-      </Draggable>
-      <div style={ HourglassStyles }><Hourglass /></div>
-      <AppBar style={{bottom: 0, top: 'unset'}}>
-        <Toolbar>
-          <Menu />
-          <Divider style={{ margin: '0 5px'}} vertical={ true } />
-          <span style={{ flexGrow: 1 }}></span>
-          <Divider style={{ margin: '0 5px'}} vertical={ true } />
-          <Clock style={{ fontWeight: 600 }} />
-        </Toolbar>
-      </AppBar>
+
+        <Draggable positionOffset={{ x: '35vw', y: '25vh' }}>
+          <Window style={{ width: 400 }}>
+            <WindowHeader>Welcome to ToRA95</WindowHeader>
+
+            <Toolbar />
+
+            <WindowContent style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexFlow: 'column wrap' }}>
+              <img alt='Such Amaze' src={ Doge } draggable={ false } />
+              <div style={{ padding: '1rem 0' }}>Wow. Such retro.</div>
+              <Hourglass style={{ display: 'block' }} />
+            </WindowContent>
+            
+          </Window>
+        </Draggable>
+
+        <AppBar style={{bottom: 0, top: 'unset'}}>
+          <Toolbar>
+
+            <Menu />
+            <Divider style={{ margin: '0 5px'}} vertical={ true } />
+
+            <span style={{ flexGrow: 1 }}></span>
+
+            <Divider style={{ margin: '0 5px'}} vertical={ true } />
+            <Clock style={{ fontWeight: 600 }} />
+
+          </Toolbar>
+        </AppBar>
+
       </ThemeProvider>
     </div>
   );
---
+++ src/components/Clock.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  state = {
+    time: new Date()
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <span>
+        {this.state.time.toLocaleTimeString()}
+      </span>
+    );
+  }
+}
+
+export default Clock;

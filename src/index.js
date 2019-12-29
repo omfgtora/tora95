fc70672ba35cf3c25e7d3f968f8db6b9dd2ba846
omfgtora <spaminol@gmail.com>
2019-12-28 20:09:13 -0800

    Add Draw. Change drag to header only.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ToRA95 from './ToRA95';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ToRA95 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
